f486df03dbf5c5fbc359c55de98ceeef896f6c18
Ramin Mammadzada <remki-design@hotmail.com>
2018-04-04 15:36:47 +0300

    result

---
+++ security webcam.py
@@ -0,0 +1,70 @@
+#if opencv was installed through home-brew on mac
+#sys.path.append('/usr/local/lib/python2.7/site-packages')
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import time
+
+from sendEmail import sendThroughEmail
+
+def rgb2gray(rgb_image):
+    gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+    return gray_image
+
+def rgb2gray1(rgb_image):
+    row,col,ch = rgb_image.shape
+
+    gray_image = np.zeros((row, col))
+
+    for i in range(row) :
+        for j in range(col):
+            gray_image[i,j] = ( rgb_image[i,j,0]*0.02989 + rgb_image[i,j,1]*0.5870 + rgb_image[i,j,2] *0.1140 ) #the algorithm i used id , G =  B*0.07 + G*0.72 + R* 0.21
+                                                                               #I found it online
+    return gray_image
+
+
+def difference(img1_gray, img2_gray, row, column):
+    print("row = ", row)
+    print("column = ", column)
+    local_difference = 0
+
+    img1_gray = img1_gray/255
+    img2_gray = img2_gray/255
+
+    counter = 0
+
+    for i in range(0, row, 10):
+        for j in range(0, column, 10):
+            local_difference = abs( np.sum(np.sum(img1_gray[i,j] - img2_gray[i,j])) )
+            if(local_difference>0.17):
+                counter = counter + 1
+    return counter
+
+def mainFunction():
+    cam = cv2.VideoCapture(0)
+    diff = 0
+    while True:
+        img1 = cam.read()[1]
+        img1_gray = rgb2gray(img1)
+
+        time.sleep(1)
+
+        img2 = cam.read()[1]
+        img2_gray = rgb2gray(img2)
+
+        #cv2.imshow("Window",img2)
+
+        row, column = img2_gray.shape
+        diff = np.abs(difference(img1_gray, img2_gray, row, column))
+        print("counter: ", diff)
+        if diff > 10:
+            print(diff)
+            cv2.imwrite("main_image.jpg", img1)
+            plt.imshow(img2_gray)
+            sendThroughEmail()
+            plt.show()
+            break
+
+mainFunction()
---
+++ sendEmail.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+ 
+def sendThroughEmail():
+    fromaddr = "WRITE YOUR EMAIL HERE"
+    toaddr = "WRITE THE EMAIL YOU WANT TO SEND TO"
+    
+    msg = MIMEMultipart()
+    
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "ROBOTIC VISION"
+    
+    body = "This is me Alemdar"
+    
+    msg.attach(MIMEText(body, 'plain'))
+    
+    filename = "main_image.jpg"
+    attachment = open("main_image.jpg", "rb")
+    
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    
+    msg.attach(part)
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "WRITE YOUR EMAIL'S PASSWORD HERE")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
